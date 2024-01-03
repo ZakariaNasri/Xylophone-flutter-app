741630ccd2c6db2babcf9dd35d7baf47168ea7f4
ZakariaNasri <zakaria.nasri@univconstantine2.dz>
2024-01-03 17:40:54 +0000

    Creating functions ! to Flex the code amd make it short. (functions Are 'buildButton' and 'lunchAudion'.)

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,49 @@ import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
+  @override
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  void lunchAudio(int audioNum) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$audioNum.wav'));
+  }
+
+  Expanded buildButton(Color color, int num) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            lunchAudio(num);
+          });
+        },
+        child: Container(
+          height: 80,
+          color: color,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () async {
-                final player = AudioPlayer();
-                player.play(AssetSource('note3.wav'));
-              },
-              child: Text('Play'),
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              buildButton(Colors.teal[100]!, 1),
+              buildButton(Colors.teal[200]!, 2),
+              buildButton(Colors.teal[300]!, 3),
+              buildButton(Colors.teal[400]!, 4),
+              buildButton(Colors.teal[500]!, 5),
+              buildButton(Colors.teal[600]!, 6),
+              buildButton(Colors.teal[700]!, 7),
+            ],
           ),
         ),
       ),
